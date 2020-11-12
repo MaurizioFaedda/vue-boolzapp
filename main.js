2d49e096675e10bcd67e5fe4e1ace2851d8ae036
Maurizio <maurizio.faedda1@gmail.com>
2020-11-12 21:11:58 +0100

    v-for e stampati data users

---
+++ main.js
@@ -0,0 +1,79 @@
+var app = new Vue ({
+    
+    el: '#root',
+
+    data:{
+
+        contacts: [
+        {
+            name: 'Mauro',
+            avatar: '_1',
+            visible: true,
+            messages: [
+
+            ],
+        },
+        {
+            name: 'Sara',
+            avatar: '_2',
+            visible: true,
+            messages: [
+
+            ],
+        },
+        {
+            name: 'John',
+            avatar: '_3',
+            visible: true,
+            messages: [
+
+            ],
+        },
+        {
+            name: 'Emy',
+            avatar: '_4',
+            visible: true,
+            messages: [
+
+            ],
+        },
+        {
+            name: 'Bob',
+            avatar: '_5',
+            visible: true,
+            messages: [
+
+            ],
+        },
+        {
+            name: 'Paul',
+            avatar: '_6',
+            visible: true,
+            messages: [
+
+            ],
+        },
+        {
+            name: 'Janis',
+            avatar: '_7',
+            visible: true,
+            messages: [
+
+            ],
+        },
+        {
+            name: 'Jim',
+            avatar: '_8',
+            visible: true,
+            messages: [
+
+            ],
+        },
+    ],
+    },
+
+    methods: {
+
+    }
+
+})
